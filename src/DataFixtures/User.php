17041cfe216c5aeb2cff5672a78f11557fd1c861
Cyril Leblanc <cyril_leblanc@outlook.fr>
2022-03-29 11:13:10 +0200

    add lorem images in fixtures

--- src/DataFixtures/User.php
+++ src/DataFixtures/User.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Image;
 use App\Entity\Category;
 use App\Entity\Post as PostEntity;
 use App\Entity\User as UserEntity;
@@ -13,6 +14,8 @@ class User extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        // remove all images before fetching them
+        exec("rm " . __DIR__ . "/../../public/img/posts/*");
         $faker = \Faker\Factory::create('fr_FR');
         for ($i = 0; $i < 10; $i++) {
             $address = (new AddressEntity())
@@ -40,6 +43,15 @@ class User extends Fixture
                     ->setPublicationDate($faker->dateTimeBetween('-1 years', 'now'))
                     ->setCategory($faker->randomElement($manager->getRepository(Category::class)->findAll()));
                 $manager->persist($post);
+                $manager->flush();
+
+                file_put_contents(__DIR__ . '/../../public/img/posts/' . $post->getId() . '-0.jpg', file_get_contents('https://api.lorem.space/image?w=300&h=300'));
+                echo "$i/10\n";
+                $image = (new Image())
+                    ->setPost($post)
+                    ->setRank(0)
+                    ->setExtension('jpg');
+                $manager->persist($image);
             }
         }
 
