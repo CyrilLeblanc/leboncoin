b6a296c0284b4384f76c0aa5f7f3162a0e4a5382
Cyril Leblanc <cyril_leblanc@outlook.fr>
2022-03-30 09:56:53 +0200

    add possibility to clear history

--- src/Controller/ResearchController.php
+++ src/Controller/ResearchController.php
@@ -7,6 +7,7 @@ use App\Repository\ResearchRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class ResearchController extends AbstractController
 {
@@ -19,5 +20,22 @@ class ResearchController extends AbstractController
             'researches' => $researchRepository->findBy(['user' => $this->getUser()], ['dateTime' => 'DESC']),
             'formObject' => $this->createForm(ResearchType::class),
         ]);
+    }
+
+    #[Route('/research/clear', name: 'research_clear')]
+    public function clear(
+        ResearchRepository $researchRepository
+    ): RedirectResponse
+    {
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
+
+        if ($user) {
+            $history = $user->getResearch();
+            foreach ($history as $research) {
+                $researchRepository->remove($research);
+            }
+        }
+        return $this->redirectToRoute('research_index');
     }
 }
